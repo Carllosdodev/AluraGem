88b8b5bc49e1ccea5e504b07a855ce51d6f2eba0
Antonio <ti.antoniocarllos@gmail.com>
2024-09-08 02:03:46 -0300

    em andamento

---
+++ app/dados.js
@@ -0,0 +1,28 @@
+let dados = [
+    {
+        titulo: 'Naruto Uzumaki',
+        descricao: 'Naruto Uzumaki é um personagem icônico do anime e mangá Naruto, criado por Masashi Kishimoto. Órfão desde a infância e estigmatizado por abrigar a Nove-Caudas, uma poderosa criatura demoníaca, Naruto cresce buscando reconhecimento e o sonho de se tornar Hokage, o líder da Vila da Folha e o ninja mais forte da aldeia.',
+        link: 'https://pt.wikipedia.org/wiki/Naruto'
+    },
+    {
+        titulo: 'Yuji Itadori',
+        descricao: 'Yuji Itadori é o protagonista do mangá e anime Jujutsu Kaisen, criado por Gege Akutami. Um estudante do ensino médio comum, Yuji vê sua vida mudar drasticamente quando ele ingere um dedo amaldiçoado para proteger seu amigo, tornando-se um recipiente para o poderoso espírito amaldiçoado Sukuna.',
+        link: 'https://pt.wikipedia.org/wiki/Yuji_Itadori'
+    },
+    {
+        titulo: 'Goku',
+        descricao: 'Goku é o protagonista da franquia Dragon Ball, criada por Akira Toriyama. Um guerreiro Saiyajin criado na Terra, Goku passa a vida treinando e buscando desafios, sempre em busca de batalhas mais fortes e aventuras emocionantes.',
+        link: 'https://pt.wikipedia.org/wiki/Goku'
+    },
+    {
+        titulo: 'Saitama',
+        descricao: 'Saitama é o protagonista do mangá e anime One-Punch Man, criado por ONE. Após anos de treinamento intenso, Saitama se tornou tão poderoso que derrota qualquer oponente com um único soco, o que o leva a uma profunda crise existencial em busca de desafios reais.',
+        link: 'https://pt.wikipedia.org/wiki/Saitama'
+    },
+    {
+     
+        titulo: 'Guts',
+        descricao: 'Guts é o protagonista da série de mangá Berserk, criada por Kentaro Miura. Um guerreiro brutal e solitário, Guts luta em um mundo sombrio e violento, marcado por batalhas épicas e temas como a sobrevivência e a busca por vingança.',
+        link: 'https://pt.wikipedia.org/wiki/Guts_(Berserk)'
+    }
+];
